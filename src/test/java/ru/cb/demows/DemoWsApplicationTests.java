062091cc80088e1732d7dc22a2e57a943e689727
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-06-07 10:58:18 +0300

    Initial commit

---
+++ src/test/java/ru/cb/demows/DemoWsApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.cb.demows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+
+}
